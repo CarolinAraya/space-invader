e9af9c708ad0848b22ef392bd49be9c89d9487d7
Carolina <caro.frontend@gmail.com>
2022-03-23 23:00:40 -0300

    modal y sonidos

---
+++ index.js
@@ -0,0 +1,6 @@
+//const open = document.querySelector('.boton');
+var sound = new Audio();
+sound.src = "./assets/sonidos/cadaver de mirlo.mp3"
+
+var sound_two = new Audio();
+sound_two.src = "./assets/sonidos/II.mp3"
